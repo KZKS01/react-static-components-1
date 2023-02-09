495f9fdb549dc996b0c8d545235076ea9b040b89
Kathy <kath.scholle@gmail.com>
2023-02-08 22:55:49 -0500

    wip: adds files & react works

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+function Menu(){
+    return(
+        <header>
+            <h3>
+                Dashboard
+                Widget
+                Reviews
+                Customers
+                Online Analysis
+                Settings
+            </h3>
+        </header>
+    );
+}
+
+export default Menu;
